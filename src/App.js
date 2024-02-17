e844bbcec1f040e00e15ea4a7662849a2ee252b8
HarshadSahu <HarshadS@emids.com>
2024-02-17 13:01:28 +0530

    My first git commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [label, setLabel] = useState(10);
+  const [runCounter, setRuncounter] = useState(false);
+
+  const handleChange = () => {
+    setRuncounter(true);
+  };
+
+  useEffect(() => {
+    if (runCounter) {
+      if (label > 0) {
+        setTimeout(() => {
+          if (label > 0) {
+            setLabel((val) => val - 1);
+          }
+        }, 1000);
+      } else {
+        setRuncounter(false);
+      }
+    }
+  }, [runCounter, label]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Hello Everyone.</h3>
+        <h5>Count is: {label}</h5>
+        <button onClick={handleChange}>Change Count</button>
       </header>
     </div>
   );
